6d73078505d619246d35461c9a8682fe774a384b
Nicholas David Wilcox <nicholas.wilcox@gmail.com>
2025-02-18 14:28:37 -0600

    added pregen readmecd

--- tests/crm.spec.ts
+++ tests/crm.spec.ts
@@ -4,31 +4,6 @@
 
 
 
-
-
-/*
- * Jira Issue: CRM-1
- * Title: (Sample) Customer Interaction Tracking
- * Acceptance Criteria:
- *  * Track and manage customer interactions and communications.
- */
-
-
-/*
- * Jira Issue: CRM-2
- * Title: (Sample) User Management
- * Acceptance Criteria:
- *  * Manage user accounts and permissions within the CRM system.
- */
-
-
-/*
- * Jira Issue: CRM-3
- * Title: (Sample) User Registration
- * Acceptance Criteria:
- *  * Implement user registration functionality.
- */
-
 // @TESTGEN - for AI generated scaffolding
 
 /**
